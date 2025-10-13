fae42a38444182df74836b630e3348b157331cef
rsa-code <ricardosoaresaraujo.business@gmail.com>
2025-10-13 10:28:41 +0100

    fix: resolve promise from draftMode before disabling draft

--- cms-contentful-app/app/api/disable-draft/route.ts
+++ cms-contentful-app/app/api/disable-draft/route.ts
@@ -1,6 +1,11 @@
 import { draftMode } from 'next/headers';
 
 export async function GET(request: Request) {
-  draftMode().disable();
+  // Use 'await' here to resolve the promise returned by draftMode()
+  const draft = await draftMode();
+
+  // Now you can call .disable() on the resolved object
+  draft.disable();
+
   return new Response('Draft mode is disabled');
 }
