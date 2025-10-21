c3793044b59790265028d094bbb0ce244c8431aa
rsa-code <ricardosoaresaraujo.business@gmail.com>
2025-10-21 10:14:53 +0100

    feat: reorder imports and update blog title for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
 import { draftMode } from 'next/headers';
+import Link from 'next/link';
 
-import Date from './date';
-import CoverImage from './cover-image';
 import Avatar from './avatar';
+import CoverImage from './cover-image';
+import Date from './date';
 import MoreStories from './more-stories';
 
 import { getAllPosts } from '@/lib/api';
@@ -13,7 +13,7 @@ function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog de TDW.
+        Blog da minha pessoa.
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
         Aplicação de exemplo para o MP1 com:{' '}
